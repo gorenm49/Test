38c616cd699eacd2c9be1ce1638864a54822da5d
Nitya <gorenityanand96@gmail.com>
2022-01-29 16:21:40 +0530

    TestCommit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and ladder game");
+
+            SnakeAndLadderGame game = new SnakeAndLadderGame();
+            game.DiceRoll();
+            game.GamePlay();
+
+            //this is for testing purpose only
+        }
+        
+    }
+}
